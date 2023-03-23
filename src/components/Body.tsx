c70207eb703512e4d20c7df90376dc8be8e32857
EmersonSineziio <emerson.sineziio@gmail.com>
2023-03-23 07:41:51 -0300

    update

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,9 +1,11 @@
+import Characteristics from "./Characteristics";
 import ShowItem from "./ShowItem";
 
 function Body() {
   return (
     <div className="body">
       <ShowItem />
+      <Characteristics />
     </div>
   );
 }
---
+++ src/components/Characteristics.tsx
@@ -0,0 +1,20 @@
+function Characteristics() {
+  return (
+    <div className="info">
+      <div className="description">
+        <p>icon</p> <span>desc</span>
+      </div>
+      <div className="description">
+        <p>icon</p> <span>desc</span>
+      </div>
+      <div className="description">
+        <p>icon</p> <span>desc</span>
+      </div>
+      <div className="description">
+        <p>icon</p> <span>desc</span>
+      </div>
+    </div>
+  );
+}
+
+export default Characteristics;
